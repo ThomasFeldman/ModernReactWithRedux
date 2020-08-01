901e51ad9d8946dd1ec0fa927edaa645c0b7e74b
ThomasFeldman <tfeldman1.31@gmail.com>
2020-08-01 11:15:53 -0500

    modifies index.js of jsx

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-// Import the React and ReactDOM Libraries
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
-//Create a react component
-const App = () => {
-    return (
-      <div>
-        <label class="label" for="name">Enter name:</label>
-        <input id="name" type="text" />
-        <button style="background-color: blue; color: white;">Submit</button>
-      </div>
-    );
-};
 
-// Take a react component and show it on the screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+// Create a react component
+
+//Take the react compnonent and show it on the screen.
